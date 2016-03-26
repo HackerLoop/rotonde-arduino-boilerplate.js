d74ee517f872e95b285f8314f71d335c25ef0161
Constantin Clauzel <stantin04@gmail.com>
2016-03-26 16:48:26 +0100

    added other actions and events

--- config.js
+++ config.js
@@ -12,7 +12,7 @@ module.exports = {
   modulePrefix: 'HAND',
   definitions: [{
     type: 'action',
-    identifier: 'HAND_FINGERS',
+    identifier: 'HAND_SET_FINGERS',
     processFields: (a, index) => {
       const cmd = _.reduce(a.data.fingers, (c, f, i) => {
         return c + ',' + Math.floor(f.position*10000)/10000 + ',' + Math.floor(f.speed*10000)/10000;
@@ -24,6 +24,30 @@ module.exports = {
         name: 'fingers',
         type: 'array',
         units: 'position: 0-1, speed: 0-1',
+      }
+    ],
+  }, {
+    type: 'action',
+    identifier: 'HAND_GET_FINGERS',
+    fields: [],
+  }, {
+    type: 'event',
+    identifier: 'HAND_FINGERS',
+    fields: [
+      {
+        name: 'fingers',
+        type: 'array',
+        units: 'position: 0-1, speed: 0-1',
+      }
+    ],
+  }, {
+    type: 'event',
+    identifier: 'HAND_FINGER_BLOCKED',
+    fields: [
+      {
+        name: 'finger',
+        type: 'number',
+        units: '',
       }
     ],
   }]
